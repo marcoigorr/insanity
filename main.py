fe263054efccd31e639e8b726660d5966f0daa0a
marcoigorr <petronio.mrc@gmail.com>
2022-01-28 16:07:56 +0100

    Perfetta, basta solo mettere 75 if else :)))

--- main.py
+++ main.py
@@ -7,33 +7,39 @@ import keyboard
 import webbrowser
 import os
 import subprocess
+import webview
 
 
+def createWindow(answer):
+    f = open("text.txt", "a+")
+    f.write(answer)
+    f.close()
+    window = webview.create_window(answer, 'text.txt', width=100, height=50, )
+    webview.start()
+    time.sleep(1)
+    os.remove("text.txt")
+
+# Yanderedev momentum
 def giveAnswer(text):
-
-
     if "What is the prefix length notation for the subnet mask 255.255.255.224?" in text:
-        f = open("text.txt", "a+") # Create txt file
-        f.write("/27") # Answer
-        f.close()
-        webbrowser.open("text.txt") # Open Notepad Window
-        time.sleep(1)
-        os.remove("text.txt") # Remove file
+        createWindow("/27") # Answer
 
     elif "How many valid host addresses are available on an IPv4 subnet that is" in text:
-        f = open("text.txt", "a+")
-        f.write("62")
-        f.close()
-        webbrowser.open("text.txt")
-        time.sleep(1)
-        os.remove("text.txt")
+        createWindow("62")
+
+    elif "Which subnet mask would be used if 5 host bits are available?" in text:
+        createWindow("255.255.255.224")
+
+    elif "A network administrator subnets the 192.168.10.0/24 network into subnets with /26" in text:
+        createWindow("4")
 
     else:
-        print("No answer or question found.")
+        createWindow("No answer or question found")
 
-
+# Importazione Pytesseract esterno; da sistemare
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
+# Dimensione della cattura
 mon = {'top': 10, 'left': 0, 'width': 1920, 'height': 600}
 
 with mss.mss() as sct:
@@ -52,4 +58,3 @@ with mss.mss() as sct:
             if cv2.waitKey(25) & 0xFF == ord('0'):
                 cv2.destroyAllWindows()
                 break
-
